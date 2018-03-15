f9b453421f945276ea889b47e2d8f8952e20aecd
joao vitor da silva braga <a2016951723@la0012217565.infocoltec.local>
2018-03-15 17:27:47 -0300

    Entrega E03

---
+++ app/src/main/java/br/ufmg/coltec/tp/e03_layout/load.java
@@ -0,0 +1,14 @@
+package br.ufmg.coltec.tp.e03_layout;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class load extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load);
+    }
+}
---
+++ app/src/main/java/br/ufmg/coltec/tp/e03_layout/previsao_tempo.java
@@ -0,0 +1,14 @@
+package br.ufmg.coltec.tp.e03_layout;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class previsao_tempo extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_previsao_tempo);
+    }
+}
